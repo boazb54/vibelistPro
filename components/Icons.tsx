a82c1c3ff787e87a99707b41537242eeff8464b9
boazb54 <59447244+boazb54@users.noreply.github.com>
2025-12-09 09:41:04 +0200

    Add files via upload

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -30,3 +30,9 @@ export const CogIcon: React.FC<{className?: string; onClick?: () => void}> = ({c
     <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
   </svg>
 );
+
+export const MicIcon: React.FC<{className?: string; onClick?: () => void}> = ({className, onClick}) => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className} onClick={onClick}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M12 18.75a6 6 0 006-6v-1.5m-6 7.5a6 6 0 01-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 01-3-3V4.5a3 3 0 116 0v8.25a3 3 0 01-3 3z" />
+  </svg>
+);
