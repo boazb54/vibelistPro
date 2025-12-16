0570623dc4024b0364361ffdd02c6a1108b80b6d
boazb54 <59447244+boazb54@users.noreply.github.com>
2025-12-17 00:54:12 +0200

    Delete textUtils.ts

--- textUtils.ts
+++
@@ -1,4 +0,0 @@
-// Helper for RTL detection (Hebrew + Arabic ranges)
-// Hebrew: \u0590-\u05FF
-// Arabic: \u0600-\u06FF
-export const isRtl = (text: string) => /[\u0590-\u05FF\u0600-\u06FF]/.test(text);
